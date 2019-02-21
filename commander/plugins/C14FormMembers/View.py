8db300a5e63a1536794fdce1b38ebeb776a0b1cf
demjanp <peter.demjan@gmail.com>
2019-02-21 13:10:20 +0100

    commit

--- commander/plugins/C14FormMembers/View.py
+++ commander/plugins/C14FormMembers/View.py
@@ -72,6 +72,24 @@ class View(*uic.loadUiType(os.path.join(os.path.dirname(__file__), "ui", "View.u
 				return cls, descr
 		return None, None
 	
+	def find_relations(self):
+		# find all possible relations
+		# returns [[cls1, rel, cls2], ...]
+		
+		relations = [] # [[cls1, rel, cls2], ...]
+		classes = []
+		for _, cls, _ in self.targets:
+			if cls not in classes:
+				classes.append(cls)
+		for cls1 in classes:
+			for rel in self.store.classes[cls1].relations:
+				if rel[0] == "~":
+					continue
+				for cls2 in self.store.classes[cls1].relations[rel]:
+					if cls2 in classes:
+						relations.append([cls1, rel, cls2])
+		return relations
+	
 	def clear_control(self, ctrl):
 		
 		if isinstance(ctrl, QtWidgets.QComboBox):
@@ -113,7 +131,7 @@ class View(*uic.loadUiType(os.path.join(os.path.dirname(__file__), "ui", "View.u
 			if isinstance(ctrl, QtWidgets.QComboBox):
 				self.update_combo(ctrl)
 	
-	def update_combo(self, ctrl):
+	def update_combo(self, ctrl, set_value = None):
 		
 		cls, descr = self.get_control_class_descr(ctrl)
 		if cls is None:
@@ -126,8 +144,26 @@ class View(*uic.loadUiType(os.path.join(os.path.dirname(__file__), "ui", "View.u
 				values.append(value)
 		if values:
 			ctrl.addItems([""] + values)
+		if set_value is not None:
+			set_value = str(set_value)
+			if set_value in values:
+				ctrl.setCurrentIndex(values.index(set_value) + 1)
+			elif values:
+				ctrl.setItemText(0, set_value)
+			else:
+				ctrl.setCurrentText(set_value)
 	
-	def add_group_entry(self, key):
+	def set_control(self, ctrl, value):
+		
+		if isinstance(ctrl, QtWidgets.QComboBox):
+			self.update_combo(ctrl, value)
+		elif isinstance(ctrl, QtWidgets.QLineEdit):
+			ctrl.setText(value)
+		elif isinstance(ctrl, QtWidgets.QPlainTextEdit):
+			ctrl.setPlainText(value)
+	
+	def add_group_entry(self, key, values = {}):
+		# values = {descr: value, ...}
 		
 		group_ctrl = self.__dict__[key]
 		
@@ -163,25 +199,117 @@ class View(*uic.loadUiType(os.path.join(os.path.dirname(__file__), "ui", "View.u
 			frame.adjustSize()
 			self.targets.append([field, cls, descr])
 			added.append(field)
-			if isinstance(field, QtWidgets.QComboBox):
-				self.update_combo(field)
+			if descr not in values:
+				if isinstance(field, QtWidgets.QComboBox):
+					self.update_combo(field)
+			else:
+				self.set_control(field, values[descr])
 		return added
+	
+	def get_selected(self):
+		
+		current = self.parent.parent.view.mdiarea.get_current()
+		if current and hasattr(current, "get_selected_objects"):
+			objects = list(current.get_selected_objects().values())
+			if len(objects) == 1:
+				return objects[0].id
+		return None
+	
+	def update_controls(self):
+		
+		def get_multi_entry(cls):
+			
+			for key in self.multi_entry:
+				for _, _, cls2, _ in self.multi_entry[key]:
+					if cls2 == cls:
+						return key
+			return None
+		
+		selected_id = self.get_selected()
+		if selected_id is None:
+			return
+		
+		# find all possible relations
+		relations = self.find_relations() # [[cls1, rel, cls2], ...]
+		collect = []
+		for cls1, rel, cls2 in relations:
+			rel_rev = self.store.reverse_relation(rel)
+			collect.append([cls2, rel_rev, cls1])
+		relations += collect
+		
+		# find multi_entry fields
+		multi_fields = {}  # {cls: key, ...}
+		for ctrl, cls, descr in self.targets:
+			if cls not in multi_fields:
+				key = get_multi_entry(cls)
+				if key is not None:
+					multi_fields[cls] = key
+		
+		# get all relevant objects
+		objects = defaultdict(list)  # {class: [object, ...], ...}
+		obj = self.store.objects[selected_id]
+		for cls in obj.classes:
+			objects[cls].append(obj)
+		while True:
+			found = False
+			for cls1, rel, cls2 in relations:
+				if cls1 in objects:
+					for obj1 in objects[cls1]:
+						if rel in obj1.relations:
+							for obj_id2 in obj1.relations[rel]:
+								obj2 = self.store.objects[obj_id2]
+								if (cls2 in obj2.classes) and ((cls2 in multi_fields) or (cls2 not in objects)) and (obj2 not in objects[cls2]):
+									objects[cls2].append(obj2)
+									found = True
+			if not found:
+				break
+		
+		# collect data from objects
+		data = {}  # {cls: {obj_id: {descr: value, ...}, ...}, ...}
+		for cls in objects:
+			data[cls] = {}
+			for obj in objects[cls]:
+				data[cls][obj.id] = {}			
+				for descr in obj.descriptors:
+					value = obj.descriptors[descr].label.value
+					if value is None:
+						continue
+					data[cls][obj.id][descr] = value
+		
+		# filter multi_fields
+		collect = {}
+		for cls in multi_fields:
+			if (cls in data) and (len(data[cls]) > 1):
+				collect[cls] = multi_fields[cls]
+		multi_fields = collect
+		
+		# fill single entry fields
+		for ctrl, cls, descr in self.targets.copy():
+			if (cls in multi_fields) or (not cls in data):
+				continue
+			obj_id = next(iter(data[cls]))
+			if descr in data[cls][obj_id]:
+				self.set_control(ctrl, data[cls][obj_id][descr])
+		
+		# fill multi entry fields
+		for cls in multi_fields:
+			key = multi_fields[cls]
+			obj_ids = list(data[cls].keys())
+			# fill first entry
+			obj_id = obj_ids.pop(0)
+			for ctrl, cls2, descr in self.targets.copy():
+				if cls2 != cls:
+					continue
+				if descr in data[cls][obj_id]:
+					self.set_control(ctrl, data[cls][obj_id][descr])
+			# fill rest of entries
+			for obj_id in obj_ids:
+				self.add_group_entry(key, data[cls][obj_id])
 	
 	def submit(self):
 		
 		# find all possible relations
-		relations = [] # [[cls1, rel, cls2], ...]
-		classes = []
-		for _, cls, _ in self.targets:
-			if cls not in classes:
-				classes.append(cls)
-		for cls1 in classes:
-			for rel in self.store.classes[cls1].relations:
-				if rel[0] == "~":
-					continue
-				for cls2 in self.store.classes[cls1].relations[rel]:
-					if cls2 in classes:
-						relations.append([cls1, rel, cls2])
+		relations = self.find_relations() # [[cls1, rel, cls2], ...]
 		
 		# collect values from form
 		values = defaultdict(lambda: defaultdict(dict)) # {cls: {idx: {descr: value, ...}, ...}, ...}
@@ -233,6 +361,9 @@ class View(*uic.loadUiType(os.path.join(os.path.dirname(__file__), "ui", "View.u
 	def on_activate(self):
 		
 		self.reset_controls()
+		self.update_controls()
+#		self.activateWindow()
+		self.raise_()
 	
 	@QtCore.pyqtSlot()
 	def on_add_entry(self):
