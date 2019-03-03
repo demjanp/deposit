1188049f0eef7fc87832d8f8708f241ad8fe8003
demjanp <peter.demjan@gmail.com>
2019-03-03 13:44:39 +0100

    commit

--- commander/Commander.py
+++ commander/Commander.py
@@ -1,20 +1,10 @@
 from deposit.commander.View import (View)
 
-from PyQt5 import (QtWidgets)
-import sys
-
 class Commander(object):
 
 	# main Deposit Commander class
 	
-	def __init__(self, model = None, start_application = True):
+	def __init__(self, model = None):
 		
-		if start_application:
-			app = QtWidgets.QApplication(sys.argv)
-
 		self.view = View(model)
-		
 		self.view.show()
-		
-		if start_application:
-			app.exec_()
