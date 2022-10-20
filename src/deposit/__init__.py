0661143c31af6097e3aaf19468c853e88a5e8089
demjanp <peter.demjan@gmail.com>
2022-10-20 15:00:17 +0200

    fixed loading legacy pickle databases

--- src/deposit/__init__.py
+++ src/deposit/__init__.py
@@ -1,8 +1,8 @@
-version_info = (1, 4, 3)
+version_info = (1, 4, 31)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'Deposit'
-__date__ = "11.10.2022"
+__date__ = "20.10.2022"
 
 from deposit.store.store import Store
 
