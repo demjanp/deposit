9f811b2f2c3107ec87928291baa9c1cd5fa8bfb9
demjanp <peter.demjan@gmail.com>
2023-02-26 23:22:13 +0100

    fixed handling of schema in PostgreSQL databases

--- src/deposit/__init__.py
+++ src/deposit/__init__.py
@@ -1,8 +1,8 @@
-version_info = (1, 4, 36)
+version_info = (1, 4, 37)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'Deposit'
-__date__ = "12.2.2023"
+__date__ = "26.2.2023"
 
 from deposit.store.store import Store
 
